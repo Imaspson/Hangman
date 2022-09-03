0b163da2f69d2252eb89a516783f5147ddbeacb8
unknown <maier.samuel99@gmail.com>
2022-09-03 14:47:10 +0300

    Added variables to store the letters from the chosen word and also to store the letters that the user has guessed.

--- hangman.py
+++ hangman.py
@@ -1,8 +1,17 @@
 import random
 from words import words
+import string
 
 def get_valid_word(words):
     word = random.choice(words)
 
     while '-' in word or ' ' in word:
         word = random.choice(word)
+
+    return word
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
