1d0e2524221ce5a4f15ff0dc0da0de690b173439
unknown <maier.samuel99@gmail.com>
2022-09-03 15:14:42 +0300

    Added a while loop that makes possible for the user to guess until the entire word has been discovered.

--- hangman.py
+++ hangman.py
@@ -17,16 +17,17 @@ def hangman():
     used_letters = set()
 
     #getting user input
-    user_letter = str(input("Guess a letter: "))
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+    while len(word_letters) > 0:
+        user_letter = str(input("Guess a letter: "))
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
 
-    elif user_letter in used_letters:
-        print("You have already used character.")
+        elif user_letter in used_letters:
+            print("You have already used character.")
 
-    else:
-        print("Invalid character. Please insert another valid one.")
+        else:
+            print("Invalid character. Please insert another valid one.")
 
 user_input = input("Type something: ")
