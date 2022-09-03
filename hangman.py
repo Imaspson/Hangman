e43a3052ca572b5c6681e3f4d7ea18c556e739cf
unknown <maier.samuel99@gmail.com>
2022-09-03 15:09:04 +0300

    Added some conditionals to better user experience, if a the word chosen by the user was already used, the user is promopted to chose another one. Same thing if the input is not valid.

--- hangman.py
+++ hangman.py
@@ -15,3 +15,18 @@ def hangman():
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
+
+    #getting user input
+    user_letter = str(input("Guess a letter: "))
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print("You have already used character.")
+
+    else:
+        print("Invalid character. Please insert another valid one.")
+
+user_input = input("Type something: ")
